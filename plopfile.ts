875ae144b4f058418ad5575834325d3357a57023
Behzad Khalifeh <wexsilon2001@protonmail.com>
2026-05-21 04:40:22 +0330

    feat: add plop to generate boilerplate

---
+++ plopfile.ts
@@ -0,0 +1,22 @@
+import type { NodePlopAPI } from 'plop';
+
+export default async function (plop: NodePlopAPI) {
+  // controller generator
+  plop.setGenerator('controller', {
+    description: 'application controller logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'controller name please',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/{{name}}.js',
+        templateFile: 'plop-templates/controller.hbs',
+      },
+    ],
+  });
+}
