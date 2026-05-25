bad2653ecb477459c1aea58c0ca14a3f466187fd
Behzad Khalifeh <wexsilon2001@protonmail.com>
2026-05-25 11:01:40 +0330

    fix: improve and , or

--- plop/helpers/operators/or.ts
+++ plop/helpers/operators/or.ts
@@ -1,7 +1,8 @@
 import type { NodePlopAPI } from 'plop';
 
 export default function logicalOr(plop: NodePlopAPI) {
-  plop.setHelper('or', function (a: boolean, b: boolean) {
-    return a || b;
+  plop.setHelper('or', function (...args: any[]) {
+    args.pop();
+    return args.some(Boolean);
   });
 }
