911314021f1f6cd4b03573618e7ae24661f6bc81
Behzad Khalifeh <wexsilon2001@protonmail.com>
2026-05-28 13:00:18 +0330

    feat: improve config

--- src/modules/config/resources/app-resource.ts
+++ src/modules/config/resources/app-resource.ts
@@ -10,56 +10,51 @@ import {
 } from 'class-validator';
 import { BooleanOrString } from '../../../common/validators/boolean-or-string';
 import { IpOrFqdn } from '../../../common/validators/ip-or-fqdn';
-import {
-  createValidatedConfig,
-  transformBoolean,
-  transformNumber,
-  transformString,
-} from '../../../common/helper/functions';
+import { createValidatedConfig } from '../../../common/helper/functions';
 import { ConfigType } from '@nestjs/config';
 
 export class AppConfig {
   @Expose({ name: 'APP__HOST' })
-  @Transform(({ value }) => transformString(value, '0.0.0.0'))
+  @Transform(({ value }) => value ?? '0.0.0.0')
   @IsString()
   @Validate(IpOrFqdn)
   host!: string;
 
   @Expose({ name: 'APP__PORT' })
-  @Transform(({ value }) => transformNumber(value, 3000))
+  @Transform(({ value }) => value ?? 3000)
   @IsNumber()
   @Min(0)
   @Max(65535)
   port!: number;
 
   @Expose({ name: 'APP__NAME' })
-  @Transform(({ value }) => transformString(value, 'NestJS-MikroOrm-Template'))
+  @Transform(({ value }) => value ?? 'NestJS-MikroOrm-Template')
   @IsString()
   name!: string;
 
   @Expose({ name: 'APP__VERSION' })
-  @Transform(({ value }) => transformString(value, '1.0.0'))
+  @Transform(({ value }) => value ?? '1.0.0')
   @IsString()
   @IsSemVer()
   version!: `${number}.${number}.${number}`;
 
   @Expose({ name: 'APP__TRUST_PROXY' })
-  @Transform(({ value }) => transformBoolean(value, true))
+  @Transform(({ value }) => value ?? true)
   @Validate(BooleanOrString)
   trustProxy!: boolean | string;
 
   @Expose({ name: 'APP__ENABLE_GLOBAL_PREFIX' })
-  @Transform(({ value }) => transformBoolean(value, true))
+  @Transform(({ value }) => value ?? true)
   @IsBoolean()
   enableGloablPrefix!: boolean;
 
   @Expose({ name: 'APP__GLOBAL_PREFIX' })
-  @Transform(({ value }) => transformString(value, 'api'))
+  @Transform(({ value }) => value ?? 'api')
   @IsString()
   globalPrefix!: string;
 
   @Expose({ name: 'APP__ENABLE_VERSION' })
-  @Transform(({ value }) => transformBoolean(value, true))
+  @Transform(({ value }) => value ?? true)
   @IsBoolean()
   enableVersion!: boolean;
 }
